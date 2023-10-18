c47177f4ce3e767151c177b931be491c20043ec8
unknown <gausulislam5@gmail.com>
2023-10-19 00:26:47 +0600

    tailwind setup daisyul setup and with navbar setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9iN3TuqS0itgHbvyyprK_aWqyyLA-6N4",
+  authDomain: "auth-firebase-context-ta-8fe6c.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-8fe6c",
+  storageBucket: "auth-firebase-context-ta-8fe6c.appspot.com",
+  messagingSenderId: "451479174856",
+  appId: "1:451479174856:web:cb24e8edf83890a4202ffd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
