4b26849c20f8af1a884561112806eb3ab778942c
unknown <gausulislam5@gmail.com>
2023-10-20 01:34:39 +0600

    update onAuthstatechange use and login and signout added

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,6 @@
 
-import React, { createContext, useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import React, { createContext, useEffect, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth'
 import app from '../firebase/firebase.config';
 
 
@@ -19,10 +19,25 @@ const AuthProvider = ({children}) => {
         return signInWithEmailAndPassword(auth,email,password);
     }
 
+    const logOut=()=>{
+        return signOut(auth)
+    }
+
+    useEffect(()=>{
+        const unsubscribe=onAuthStateChanged(auth,currentUser=>{
+            console.log('auth state change',currentUser);
+            setUser(currentUser);
+        })
+        return () =>{
+            unsubscribe();
+        }
+    },[])
+
     const AuthInfo={
         user,
         createUser,
-        SignIn
+        SignIn,
+        logOut
     }
 
     return (
